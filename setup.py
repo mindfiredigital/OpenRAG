64cd46ee9eb8b27a2f8f4e66d52392041c0ae5f1
Abdulla <abdulla.ansari@mindfiresolutions.com>
2024-09-05 15:49:54 +0530

    fix: update setup files

--- setup.py
+++ setup.py
@@ -1,64 +1,44 @@
-import sys
-from setuptools import setup
 
-__version__ = "0.0.1"
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name="open-rag",
-    version=__version__,
+    name="open-rag",  # Your package name 
+    use_scm_version=True,  # Use setuptools-scm to handle versioning
+    # version = "1.1.2",
+    use_scm_version={"local_scheme": "no-local-version"}, 
+   
+# Avoid using local versions
+    setup_requires=["setuptools>=42", "setuptools-scm"],  # Use attr to get the version from your package
     description='''An open-source Generative AI (GenAI) application designed to provide users
                   with the flexibility and power to work with various open-source large
                   language models (LLMs). This application serves as a versatile tool, 
                   enabling users to interact with and query their data using different
                   AI models and vector databases.''',
-    long_description='''An open-source Generative AI (GenAI) application designed to
-                       provide users with the flexibility and power to work with
-                       various open-source large language models (LLMs). This
-                       application serves as a versatile tool, enabling users to
-                       interact with and query their data using different AI models
-                       and vector databases.
-
-                    Key Features:
-                    Support for All Open-Source LLM Models: The application is built
-                    with comprehensive support for all major open-source LLMs. This
-                    allows users to select from a wide range of models, ensuring
-                    they can choose the one that best fits their specific needs and use cases.
-
-                    Integration with Multiple Open Source and Free to use Vector Databases:
-                    The application currently supports three popular vector databases—Chroma,
-                    FAISS, and Qdrant—with plans to expand this list further. This feature
-                    enables users to leverage different indexing and search capabilities,
-                    enhancing the efficiency and accuracy of information retrieval within their data.
-
-                    PDF File Upload and Data Querying: Users can easily upload their PDF
-                    files to the application. Once uploaded, they can select their preferred
-                    LLM and vector database to create a collection. This collection serves
-                    as a structured repository of the uploaded data, which users can then
-                    query to extract insights or generate responses based on the content of the PDF.
-
-                    Persistent Collection Names for Reusability: Upon uploading a PDF,
-                    the application generates a unique collection name. This name is
-                    crucial as it allows users to reference their data in future
-                    sessions without needing to re-upload the PDF. Users can simply
-                    choose the previously generated collection name, along with any
-                    LLM and vector database of their choice, to query the same data seamlessly.
-
-                    Consistency in Vector Database Usage: The vector database
-                    selected during the PDF upload process is tightly coupled
-                    with the data collection. When querying the data, the
-                    application ensures that only the initially chosen vector
-                    database is used. Attempting to switch to a different vector
-                    database for querying will result in an error, maintaining
-                    consistency and accuracy in data retrieval.''',
-    author="Abdulla Ansari",
+    author="Mindfire Digital LLP",
     author_email="abdulla.ansari@mindfiresolutions.com",
-    maintainer="Chirag Patil",
-    maintainer_email="Chiragp@mindfiresolutions.com"
+    packages=find_packages(where="."),  # Specifies that packages are in the root directory
+    package_dir={"": "."},  # Root directory is the package directory
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # or "text/x-rst" if using reStructuredText
+    install_requires=[
+        "langchain",
+        "langchain_experimental",
+        "langchain_openai",
+        "langchain_community",
+        "langchain_qdrant",
+        "langchain_huggingface",
+        "qdrant_client",
+        "PyPDF2",
+        "fastapi",
+        "gpt4all"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
 )
-
-
-try:
-    from semantic_release import setup_hook
-    setup_hook(sys.argv)
-except ImportError:
-    pass
